0c9442ef10eac4d2db9baa02cfe44f1a1ae840f3
git-shashwat <shashwat824@gmail.com>
2020-10-16 23:20:33 +0530

    added footer

---
+++ app/components/footer/footer.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './footer.styles.scss';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p>Developed with ❤️ and ☕ by Shashwat |&nbsp;</p>
+            <p>Find me on <a href="https://www.linkedin.com/in/shashwattyagi/" target="_blank" rel="noreferrer">LinkedIn </a> |&nbsp;</p>
+            <p>Drop a 👋 on <a href="https://twitter.com/RxShashwat" target="_blank" rel="noreferrer"> Twitter </a></p>
+        </div>
+    );
+};
+
+export default Footer;
--- app/routes/Pages/home-page/homepage.component.jsx
+++ app/routes/Pages/home-page/homepage.component.jsx
@@ -4,7 +4,6 @@ import { compose } from 'redux';
 import {
     Row,
     Col,
-    Container,
 } from 'reactstrap';
 import { setupPage } from '../../../components/Layout/setupPage';
 
@@ -13,28 +12,32 @@ import "./homepage.styles.scss";
 import Profile from '../../../components/profile/profile.component';
 import Yellove from '../../../components/yellove/yellove.component.jsx';
 import GenDash from '../../../components/gendash/gendash.component.jsx';
+import Footer from '../../../components/footer/footer.component.jsx';
 
 const HomePage = () => {
 
     return (
-        <div className="main-container">
-            <Row>
-                <Col lg={ 3 } md={ 4 } className="order-md-2">                
-                    <div className="mb-3">
-                        <div className="hr-text hr-text-left mt-2 mb-1">
-                            <h4 className="text-white"><b>Your Interests 🚀</b></h4>
+        <div>
+            <div className="main-container">
+                <Row>
+                    <Col lg={ 3 } md={ 4 } className="order-md-2">                
+                        <div className="mb-3">
+                            <div className="hr-text hr-text-left mt-2 mb-1">
+                                <h4 className="text-white"><b>Your Interests 🚀</b></h4>
+                            </div>
+                            <Profile />                
                         </div>
-                        <Profile />                
-                    </div>
-                </Col>            
-                <Col lg={ 9 } md={8} className="order-md-1">
-                    <div className="hr-text hr-text-left mt-2 mb-1">
-                        <h4 className="text-white"><b>We miss you, Vipul. Here&#39;s why you should come back...</b></h4>
-                    </div>
-                    <Yellove />
-                </Col>
-            </Row>
-            <GenDash />
+                    </Col>            
+                    <Col lg={ 9 } md={8} className="order-md-1">
+                        <div className="hr-text hr-text-left mt-2 mb-1">
+                            <h4 className="text-white"><b>We miss you, Vipul. Here&#39;s why you should come back...</b></h4>
+                        </div>
+                        <Yellove />
+                    </Col>
+                </Row>
+                <GenDash />        
+            </div>
+            <Footer />
         </div>
     )
 };
